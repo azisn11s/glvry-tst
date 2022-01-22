5f8583622aab9111dfdbf07fe05c1c627708c038
Azis Novian <azisnovian.kom48@gmail.com>
2022-01-23 00:59:42 +0700

    Init product component

---
+++ components/MyProduct.vue
@@ -0,0 +1,35 @@
+<template>
+	<b-col>
+		<b-card no-body class="overflow-hidden" style="max-width: 500px; max-height: 200px">
+			<b-row no-gutters>
+				<b-col md="4" class="pl-3 pt-3">
+					<b-card-img
+						src="https://picsum.photos/400/400/?image=20"
+						alt="Image"
+						class="rounded-2"
+					></b-card-img>
+				</b-col>
+				<b-col md="8">
+					<b-card-body title="Horizontal Card" class="pt-3">
+						<b-card-text class="text-muted">
+							Nasi Putih + Udang goreng saos telur asin
+						</b-card-text>
+					</b-card-body>
+                    <b-card-footer class="d-flex justify-content-end bg-white border-white">
+                        <b-button size="sm" variant="success">
+                            <b-icon icon="plus" font-scale="1.5" />
+                        </b-button>
+                    </b-card-footer>
+				</b-col>
+			</b-row>
+		</b-card>
+	</b-col>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,101 +4,24 @@
 
 		<!-- <main role="main"> -->
 		<MyJumbotron />
-    <!-- <MyCarousel /> -->
+		<!-- <MyCarousel /> -->
 
 		<MyMenu />
 		<!-- </main> -->
 
-		<div class="ml-0 mr-0 bg-light" style="">
-			<div class="container mt-100">
-				Competently coordinate an expanded array of action items before
-				bleeding-edge web-readiness. Assertively scale corporate core
-				competencies without prospective e-services. Completely supply
-				empowered human capital whereas ubiquitous sources. Conveniently
-				predominate extensive imperatives without exceptional
-				functionalities. Compellingly administrate professional "outside the
-				box" thinking whereas virtual deliverables. Progressively reinvent
-				cross functional products rather than ethical channels.
-				Appropriately reconceptualize granular communities and stand-alone
-				scenarios. Continually maximize fully researched testing procedures
-				and bleeding-edge customer service. Collaboratively exploit parallel
-				e-services whereas timely intellectual capital. Proactively
-				reintermediate vertical e-commerce without cooperative products.
-				Credibly envisioneer performance based human capital vis-a-vis
-				focused information. Globally repurpose web-enabled sources rather
-				than tactical resources. Phosfluorescently empower market
-				positioning portals after customer directed imperatives.
-				Collaboratively supply maintainable customer service with
-				functionalized results. Holisticly administrate customized e-tailers
-				and high-payoff applications. Completely evolve ethical ideas and
-				front-end infrastructures. Rapidiously integrate equity invested web
-				services with team building benefits. Proactively deploy covalent
-				solutions after out-of-the-box supply chains. Rapidiously
-				envisioneer extensible catalysts for change through business
-				solutions. Conveniently seize strategic core competencies after
-				standardized functionalities. Objectively foster value-added
-				outsourcing through market-driven "outside the box" thinking.
-				Competently procrastinate best-of-breed total linkage rather than
-				pandemic materials. Rapidiously maintain best-of-breed
-				infrastructures whereas backend ROI. Energistically cultivate highly
-				efficient partnerships rather than reliable initiatives. Seamlessly
-				harness focused solutions via team building expertise. Dynamically
-				embrace backward-compatible e-markets after excellent alignments.
-				Competently expedite extensible models vis-a-vis optimal
-				information. Holisticly innovate goal-oriented systems rather than
-				global leadership skills. Uniquely transform proactive strategic
-				theme areas whereas maintainable partnerships. Assertively
-				procrastinate efficient quality vectors after intuitive e-markets.
-				Efficiently pontificate principle-centered best practices via
-				stand-alone e-markets. Globally unleash unique relationships without
-				2.0 ROI. Synergistically redefine robust value whereas user-centric
-				infomediaries. Energistically disseminate parallel results vis-a-vis
-				state of the art total linkage. Efficiently fashion exceptional
-				catalysts for change whereas interactive infomediaries. Quickly
-				harness highly efficient communities via professional materials.
-				Completely deploy adaptive opportunities without cutting-edge
-				sources. Competently formulate team building methodologies whereas
-				next-generation testing procedures. Distinctively simplify highly
-				efficient meta-services before strategic niches. Dynamically network
-				bleeding-edge information with technically sound users.
-				Monotonectally target intermandated methodologies and equity
-				invested architectures. Progressively exploit backend alignments via
-				progressive opportunities. Seamlessly synergize team driven human
-				capital and reliable manufactured products. Conveniently
-				myocardinate inexpensive action items for compelling e-business.
-				Distinctively optimize user friendly infomediaries with premium
-				technology. Conveniently maintain 24/365 architectures and installed
-				base scenarios. Synergistically network standards compliant customer
-				service after seamless outsourcing. Interactively incubate equity
-				invested mindshare whereas proactive "outside the box" thinking.
-				Appropriately disseminate seamless meta-services through superior
-				technology. Professionally extend world-class interfaces whereas go
-				forward e-tailers. Appropriately enable user friendly technology
-				vis-a-vis seamless alignments. Continually pursue inexpensive
-				markets for team building niches. Distinctively maximize
-				backward-compatible total linkage rather than worldwide platforms.
-				Appropriately redefine viral infomediaries via stand-alone strategic
-				theme areas. Globally whiteboard principle-centered expertise before
-				cost effective web services. Compellingly leverage other's standards
-				compliant meta-services vis-a-vis front-end ROI. Quickly
-				recaptiualize functionalized "outside the box" thinking with
-				leading-edge action items. Compellingly administrate vertical
-				benefits with extensive benefits. Enthusiastically revolutionize
-				corporate markets without alternative schemas. Phosfluorescently
-				iterate multifunctional best practices through bricks-and-clicks
-				intellectual capital. Authoritatively fashion mission-critical
-				platforms vis-a-vis client-focused infomediaries. Efficiently
-				strategize synergistic human capital whereas frictionless processes.
-				Rapidiously fashion multifunctional relationships without
-				enterprise-wide process improvements. Authoritatively unleash
-				one-to-one partnerships with interoperable quality vectors.
-				Compellingly extend interactive opportunities with progressive
-				niches. Uniquely formulate functionalized solutions via
-				multidisciplinary potentialities. Monotonectally redefine robust
-				models for just in time leadership. Conveniently myocardinate
-				leveraged technology through extensible e-commerce.
-			</div>
-		</div>
+		<section class="bg-light">
+			<b-container class="products-container">
+        <h3 class="category-title">Aigo</h3>
+				<b-row>
+					
+          <MyProduct />
+          <MyProduct />
+          <MyProduct />
+
+          
+				</b-row>
+			</b-container>
+		</section>
 	</div>
 </template>
 
@@ -111,5 +34,14 @@ export default {
 <style lang="css" scoped>
 .sticky-header {
 	top: 68px;
+}
+.products-container {
+	display: block;
+	padding-top: 5%;
+	padding-bottom: 5%;
+}
+.category-title {
+  font-weight: bold;
+  margin-bottom: 3%;
 }
 </style>
